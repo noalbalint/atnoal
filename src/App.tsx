dac43e2b6b8b01f4f392e8930e6b25b45c2efb67
Noal Balint <noal.balint@echosec.net>
2023-05-01 11:23:02 +0200

    v0.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+function App(): React.ReactElement {
   const [count, setCount] = useState(0)
 
   return (
