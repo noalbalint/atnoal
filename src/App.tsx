bbf665a3db9a33201a27381302f28a7c6bfdb633
Noal Balint <noal.balint@echosec.net>
2023-09-23 19:15:40 +0200

    add tailwind + update home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { useState } from 'react'
 import './App.css'
 
 function App(): React.ReactElement {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Noals website</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <p className='font-bold pb-10 text' style={{ minWidth: '300px' }}>Hi, you've reached Noal</p>
+      <div className='flex flex-row w-full justify-between'>
+        <span>phone</span>
+        <span>+43 699 819 483 46</span>
+      </div>
+      <br />
+      <div className='flex flex-row w-full justify-between'>
+        <span>email</span>
+        <span>noalbalint@gmail.com</span>
       </div>
     </>
   )
