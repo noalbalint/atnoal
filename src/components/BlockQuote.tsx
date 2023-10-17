daa610a292996fda2b243589eca2fbb70a0d0f7a
Noal Balint <noal.balint@echosec.net>
2023-10-17 16:58:22 +0200

    update quote styling

--- src/components/BlockQuote.tsx
+++ src/components/BlockQuote.tsx
@@ -12,20 +12,20 @@ export default function BlockQuote({
   return (
     <div className={`${className} relative`}>
       <img className='absolute top-0 left-32 w-32 opacity-10' src={quoteOpen} />
-       <p className='text-left pt-6 px-36'>
+       <p className='text-left pt-6 pb-2 px-36'>
         Noal is one of those people you always want on your team. He is always eager to mentor and learn from others. He aims for excellence while understanding that trade-offs are sometimes needed. <br/><br/>
         While I worked with him, Noal led many different features from start to finish: designing, delegating, and overseeing the completion of the feature, and coding many of the tickets himself. <br/><br/>
         Noal was also a pleasure to work with. He would organize meetings when requirements were not precise and bring with him a can-do and positive attitude.
-        I would be glad to work with him again if the opportunity arises!
-        <a
-          href="https://www.linkedin.com/in/joan-orrit-palau/"
-          target='_blank'
-          style={{ color, marginLeft: '1rem' }}
-        >
-          - Joan Orrit Palau
-        </a>
+        I would be glad to work with him again if the opportunity arises.
       </p>
       <img className='absolute bottom-0 right-32 w-32 opacity-10 font-medium' src={quoteClose} />
+      <a
+          href="https://www.linkedin.com/in/joan-orrit-palau/"
+          target='_blank'
+          style={{ color }}
+        >
+          - Joan Orrit Palau
+      </a>
     </div>
   )
 }
--- src/components/BlockQuoteMobile.tsx
+++ src/components/BlockQuoteMobile.tsx
@@ -15,7 +15,7 @@ export default function BlockQuote({
           Noal is one of those people you always want on your team. He is always eager to mentor and learn from others. He aims for excellence while understanding that trade-offs are sometimes needed. <br/><br/>
           While I worked with him, Noal led many different features from start to finish: designing, delegating, and overseeing the completion of the feature, and coding many of the tickets himself. <br/><br/>
           Noal was also a pleasure to work with. He would organize meetings when requirements were not precise and bring with him a can-do and positive attitude.
-          I would be glad to work with him again if the opportunity arises!
+          I would be glad to work with him again if the opportunity arises.
         </p>
         <img className='absolute bottom-0 right-10 w-32 opacity-10 font-medium' src={quoteClose} />
       </div>
