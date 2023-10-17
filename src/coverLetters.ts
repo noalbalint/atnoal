ca6a4663344b2ac28bfe09386842751e1820a127
Noal Balint <noal.balint@echosec.net>
2023-10-17 17:10:45 +0200

    copy changes

--- src/coverLetters.ts
+++ src/coverLetters.ts
@@ -1,9 +1,9 @@
 
 export const parkside = [
-  `I am a frontend software developer with over 4 years of professional experience building enterprise-grade dynamic web apps using primarily TypeScript, Vue.js, and Less CSS.
+  `I am a product-oriented software developer with over 4 years of professional experience building enterprise-grade dynamic web apps using primarily TypeScript, Vue.js, and Less CSS.
   I love converting complicated problems into intuitive interfaces and delightful user experiences.`,
   `Originally from Canada, I have permanently relocated to Graz to be with my Austrian partner.
-  After 3 years of working remotely and 4+ years in the same codebase, Parkside's Graz-based HQ and diverse product portfolio is exactly the change I have been looking for.`,
+  After 3 years of working remotely and 4 years in the same codebase, Parkside's Graz-based HQ and diverse product portfolio is exactly the change I have been looking for.`,
   `To demonstrate my ability to quickly learn new tools, I chose to build this website with a tech stack that is completely new to me: Vite, React, Styled Components, and Tailwind CSS.`,
   `Polishing and tweaking is my favorite part of the job, and your motto of "it's done, never finished" deeply resonates with me.
   It would be a great pleasure to join your team, in any capacity.`,
@@ -29,9 +29,9 @@ export const capito = [
 ];
 
 export const grazGeneral = [
-  `I am a frontend software developer with over 4 years of professional experience building enterprise-grade dynamic web apps using TypeScript, Vue.js, and Less CSS,
+  `I am a product-oriented software developer with over 4 years of professional experience building enterprise-grade dynamic web apps using TypeScript, Vue.js, and Less CSS,
   with unit tests in Jest and end-to-end tests in Cypress.`,
-  `After 3 years of working remotely and 4+ years in the same codebase, I am looking for new opportunities in the European market, where I can
+  `After 3 years of working remotely and 4 years in the same codebase, I am looking for new opportunities in the European market, where I can
   explore new challenges and work in the same office (or even timezone) as my colleagues.`,
   `While I can manage some light backend/fullstack work when required, frontend development and UI/UX is my main interest:
   I love transforming complicated problems into intuitive interfaces and delightful user experiences`,
