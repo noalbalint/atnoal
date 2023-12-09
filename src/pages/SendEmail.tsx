385d9b5536868ca92e1705f757af73679d86b612
Noal Balint <noal.balint@echosec.net>
2023-12-09 18:57:19 +0100

    add 'docs' page for send-email

--- src/pages/SendEmail.tsx
+++ src/pages/SendEmail.tsx
@@ -1,32 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-export default function SendEmail(): React.ReactElement {
-  const [text, setText] = useState('Opening email client...');
-
-  useEffect(() => {
-    const mailtoLink = window.location.href.match(/mailto(.*)/);
-
-    if (mailtoLink !== null) {
-      console.log(mailtoLink[0]);
-      window.location.href = mailtoLink[0]
-    } else {
-      console.log('outputting text');
-      setText('Did you forget to provide a mailto link?');
-    }
-  }, []);
-
-  /**
-   * To use this, just provide a standard mailto link as the query param.
-   *
-   * You can generate it with something like this: https://www.mailto.link/
-   *
-   * eg: noalbalint.com/send-email?mailto:melanie.joly@parl.gc.ca?cc=justin.trudeau@parl.gc.ca&subject=Ceasefire%20Now
-   */
+function CreateEmail(): React.ReactElement {
   return (
     <>
-      <div className='flex items-center justify-center text-lg'>
-        { text }
+      <div className='flex flex-col items-center content-center justify-center text-lg'>
+
+        <ul className='w-[450px]'>
+          <h1 className='text-2xl pb-4 text-left'>How to share a prewritten email link:</h1>
+          <li className='text-left'>
+            <span> 1. Generate a 'mailto' link with <a href="https://mailtolinkgenerator.com/" target='_blank'> mailto link generator</a></span>
+          </li>
+          <li className='text-left'>
+            <span> 2. Paste the generated link to the end of this URL.</span>
+            <br />
+            <span className='pl-4'> Eg: <a href="noalbalint.com/send-email?[yourlink]" target='_blank'> noalbalint.com/send-email?mailto:asdf123</a></span>
+          </li>
+          <li className='text-left'>
+            <span>3. Use <a href="https://tinyurl.com/app" target='_blank'> tinyurl </a> to shorten it (optional)</span>
+          </li>
+          <li className='text-left'>
+            <span>4. Share!</span>
+          </li>
+        </ul>
       </div>
     </>
   )
+}
+
+export default function SendEmailWrapper(): React.ReactElement {
+  const mailtoLink = window.location.href.match(/mailto(.*)/);
+  const isMailtoLinkSet = mailtoLink !== null;
+
+  if (isMailtoLinkSet) {
+    window.location.href = mailtoLink[0];
+    return (
+      <div className='flex items-center justify-center text-lg'>
+        <span> Opening email client... </span>
+      </div>
+    )
+  } else {
+    if (!window.location.href.includes('?')) {
+      window.location.href += '?';
+    }
+    return CreateEmail();
+  }
 }
