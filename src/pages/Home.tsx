c8f8c290c5b371106fd352532158295df99e1b9e
Noal Balint <noalbalint@gmail.com>
2025-01-01 16:47:51 -0800

    update phone number

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ export default function Home(): React.ReactElement {
         <p className='font-bold pb-10 text' style={{ minWidth: '300px' }}>Hi, you've reached Noal</p>
         <div className='flex flex-row w-full justify-between'>
           <span>phone</span>
-          <span>+43 699 819 483 46</span>
+          <span>+1 250 538 7329</span>
         </div>
         <br />
         <div className='flex flex-row w-full justify-between'>
